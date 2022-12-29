5c97a5f6289f3f8d6271b324e0a0949fa3dd877b
ASprogram <95154468+ASprogram@users.noreply.github.com>
2022-12-29 20:07:00 +0900

    Update class_Settings.js

--- js/class_Settings.js
+++ js/class_Settings.js
@@ -1,12 +1,12 @@
-class Settings{
+class Settings {
     /**
      * {Number} unit_time :ユーザの単位時間(例:15分, 30分, 45分, 60分)
      */
     constructor() {
-        this.unit_time = null;
+        this.unit_time = 15 * 60 * 1000; // 15分に設定
     }
-    
+
     setUnitTime(time) {
-        this.unit_time = time*60*1000;
+        this.unit_time = time * 60 * 1000;
     }
 }
