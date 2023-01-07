bc0905fe39b4441f7e926bb26d3410b06272660a
BlanketStudioISHII <k_ishii@blanket-studio.com>
2023-01-07 21:29:22 +0900

    merge後調整

--- js/get_tasks.js
+++ js/get_tasks.js
@@ -21,44 +21,47 @@ const docsnap = await getDoc(userRef);
 
 export let all_tasks = [];
 
-for (let i = 0; i < Object.keys(docsnap.data()).length; i++) {
-  let data = docsnap.data()[Object.keys(docsnap.data())[i]];
-  let task;
-  if (data[8] == null) {
-    task = new Task(
-      data[0],
-      data[1],
-      data[2],
-      data[3],
-      data[4],
-      data[5],
-      data[6],
-      data[7],
-      data[8],
-      data[9],
-      data[10],
-      data[11],
-      [[data[12], data[13]]]
-    );
-  } else {
-    task = new Task(
-      data[0],
-      data[1],
-      data[2],
-      data[3],
-      data[4],
-      data[5],
-      data[6],
-      data[7],
-      data[8].seconds * 1000,
-      data[9],
-      data[10],
-      data[11],
-      [[data[12], data[13]]]
-    );
+if (docsnap.data() != null)  {
+  
+  for (let i = 0; i < Object.keys(docsnap.data()).length; i++) {
+    let data = docsnap.data()[Object.keys(docsnap.data())[i]];
+    let task;
+    if (data[8] == null) {
+      task = new Task(
+        data[0],
+        data[1],
+        data[2],
+        data[3],
+        data[4],
+        data[5],
+        data[6],
+        data[7],
+        data[8],
+        data[9],
+        data[10],
+        data[11],
+        [[data[12], data[13]]]
+      );
+    } else {
+      task = new Task(
+        data[0],
+        data[1],
+        data[2],
+        data[3],
+        data[4],
+        data[5],
+        data[6],
+        data[7],
+        data[8].seconds * 1000,
+        data[9],
+        data[10],
+        data[11],
+        [[data[12], data[13]]]
+      );
+    }
+    all_tasks.push(task);
+    // console.log(docsnap.data()[Object.keys(docsnap.data())[i]]);
   }
-  all_tasks.push(task);
-  // console.log(docsnap.data()[Object.keys(docsnap.data())[i]]);
 }
 
 //data[8]가 널이 되는거 수정
--- js/index.js
+++ js/index.js
@@ -106,20 +106,43 @@ var i = 0;
 for (const task of all_tasks) {
   i++;
   var task_container = document.createElement("div");
-  task_container.setAttribute("id",task.id.toString());
+  task_container.setAttribute("id", task.id.toString());
   task_container.classList.add("task_container");
   task_container.innerHTML = `
         <h5>${task.name}</h5>
             `;
   for (const time of task.specified_time) {
-    task_container.innerHTML += `
-        <p>実施日：${time[0]} -> ${time[1]}</p>
-            `;
-  }
-  task_container.innerHTML += `
-       <p>締切日：${task.deadline}</p>
-       `;
+    if (time[0] != null) {
+      let time_0 = new Date(time[0]["seconds"] * 1000);
+      let time_1 = new Date(time[0]["seconds"] * 1000);
 
+      let year_0 = time_0.getFullYear();
+      let month_0 = time_0.getMonth() + 1;
+      let date_0 = time_0.getDate();
+      let hour_0 = time_0.getHours();
+      let minute_0 = time_0.getMinutes();
+      let year_1 = time_1.getFullYear();
+      let month_1 = time_1.getMonth() + 1;
+      let date_1 = time_1.getDate();
+      let hour_1 = time_1.getHours();
+      let minute_1 = time_1.getMinutes();
+      task_container.innerHTML += `
+        <p>実施日：${year_0}/${month_0}/${date_0}/${hour_0}:${minute_0}~${year_1}/${month_1}/${date_1}/${hour_1}:${minute_1}</p>
+            `;
+    }
+
+  }
+  if (task.deadline != null) {
+    let time_d = new Date(task.deadline);
+    let year_d = time_d.getFullYear();
+    let month_d = time_d.getMonth() + 1;
+    let date_d = time_d.getDate();
+    let hour_d = time_d.getHours();
+    let minute_d = time_d.getMinutes();
+    task_container.innerHTML += `
+       <p>締切日：${year_d}/${month_d}/${date_d}/${hour_d}:${minute_d}</p>
+       `;
+  }
 
   document.getElementById("task_list_container").appendChild(task_container);
 }
