fa57f0cd6bc285958b38d65b6dffe789a374d21d
BlanketStudioISHII <k_ishii@blanket-studio.com>
2023-01-07 23:58:48 +0900

    .

--- js/get_tasks.js
+++ js/get_tasks.js
@@ -21,12 +21,20 @@ const docsnap = await getDoc(userRef);
 
 export let all_tasks = [];
 
-if (docsnap.data() != null)  {
-  
+if (docsnap.data() != null) {
+
   for (let i = 0; i < Object.keys(docsnap.data()).length; i++) {
     let data = docsnap.data()[Object.keys(docsnap.data())[i]];
     let task;
-    
+
+    //コンストラクターのspecified_timeを除いた引数の数
+    //統合後変えるよ！！！
+    let number_of_parameter = 12;
+    let for_times = (data.length - number_of_parameter) / 2;
+    let arr_specified_time = [];
+    for (let i = 0; i < for_times; i++) {
+      arr_specified_time.push([data[number_of_parameter + i * 2], data[number_of_parameter + i * 2 + 1]]);
+    }
     if (data[8] == null) {
       task = new Task(
         data[0],
@@ -41,7 +49,7 @@ if (docsnap.data() != null)  {
         data[9],
         data[10],
         data[11],
-        [[data[12], data[13]]]
+        arr_specified_time
       );
     } else {
       task = new Task(
@@ -57,7 +65,7 @@ if (docsnap.data() != null)  {
         data[9],
         data[10],
         data[11],
-        [[data[12], data[13]]]
+        arr_specified_time
       );
     }
     all_tasks.push(task);
